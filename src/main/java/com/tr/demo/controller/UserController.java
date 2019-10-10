58081a1a579108d0c543d5f32f801ffaf09386b8
Trangle <407833@qq.com>
2019-10-10 15:20:32 +0800

    update BaseService

--- src/main/java/com/tr/demo/controller/UserController.java
+++ src/main/java/com/tr/demo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.tr.demo.controller;
 
+import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.tr.demo.dto.User;
 import com.tr.demo.service.IUserService;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author trangle
@@ -22,22 +24,29 @@ public class UserController {
     @Autowired
     private IUserService userService;
 
+    @PostMapping("/test")
+    @ApiOperation(value = "新增用户")
+    public Boolean insertUser(@RequestBody Map<String,Object> map) {
+        return true;
+    }
+
     @PostMapping
     @ApiOperation(value = "新增用户")
-    public User insertUser(@RequestBody User user) {
-        return userService.insertSelective(user);
+    public Boolean insertUser(@RequestBody User user) {
+        return userService.insertSelective(user) > 0;
     }
 
     @GetMapping
     @ApiOperation(value = "分页查询用户")
     public List<User> selectUser(User user, @ApiIgnore PageInfo pageInfo) {
-        return userService.select(user, pageInfo);
+        PageHelper.startPage(pageInfo.getPageNum(), pageInfo.getPageSize());
+        return userService.select(user);
     }
 
     @PutMapping
     @ApiOperation(value = "更新用户")
-    public User updateUser(@RequestBody User user) {
-        return userService.updateByPrimaryKeySelective(user);
+    public Boolean updateUser(@RequestBody User user) {
+        return userService.updateByPrimaryKeySelective(user) > 0;
     }
 
     @DeleteMapping
--- src/main/java/com/tr/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/tr/demo/service/impl/UserServiceImpl.java
@@ -3,13 +3,11 @@ package com.tr.demo.service.impl;
 import com.tr.demo.dto.User;
 import com.tr.demo.service.IUserService;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author trangle
  */
 @Service
-@Transactional(rollbackFor = Exception.class)
 public class UserServiceImpl extends BaseServiceImpl<User> implements IUserService {
 }
